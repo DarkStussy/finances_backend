4f9bb90b1c2d62ee0f7151be98814bdc7e0bea6b
Stanislav <chornij.stas@gmail.com>
2023-03-25 17:39:21 +0200

    added cors middleware

--- api/__main__.py
+++ api/__main__.py
@@ -5,6 +5,7 @@ import uvicorn
 
 from fastapi import APIRouter
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from starlette.middleware.cors import CORSMiddleware
 
 from api import v1
 from api.config import load_config
@@ -27,6 +28,17 @@ def main():
 
     v1.dependencies.setup(app, api_router_v1, async_session, config, client)
     v1.routes.setup_routers(api_router_v1)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost",
+            "http://localhost:8000",
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     main_api_router = APIRouter(prefix='/api')
     main_api_router.include_router(api_router_v1, prefix='/v1')
