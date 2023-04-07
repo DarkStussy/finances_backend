15b5a54edf6a730cc5fe9e429c318f25cd2ce7dd
Stanislav <chornij.stas@gmail.com>
2023-04-07 19:22:43 +0300

    change max length of titles

--- api/v1/models/request/transaction_category.py
+++ api/v1/models/request/transaction_category.py
@@ -10,4 +10,4 @@ class TransactionCategoryCreate(BaseModel):
 
 class TransactionCategoryChange(BaseModel):
     id: int
-    title: str = Field(min_length=3, max_length=100)
+    title: str = Field(min_length=3, max_length=50)
